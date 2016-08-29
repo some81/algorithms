9a4d77deb820f2deecaafbd729a8ae6e12efbb45
Alexander Todorov <atodorov@hearst.com>
2016-08-29 11:30:48 -0400

    update

--- anagrams.php
+++ anagrams.php
@@ -36,3 +36,17 @@ print_r(anagram($array));
 //time complexity : O(n log n)  (there's sorting algorithum )
 
 
+
+//second way
+function is_anagram($a, $b) {
+    return(count_chars($a, 1) == count_chars($b, 1));
+}
+
+$a = 'argentino';
+$b = 'ignorante';
+echo is_anagram($a,$b);   // output: 1
+
+$a = 'batman';
+$b = 'barman';
+echo is_anagram($a,$b);  // output (empty):
+
