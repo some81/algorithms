317c5dc68cb03162a0fac0d286ad802423359e49
Alexander Todorov <atodorov@Alex-Retina-MBpro.local>
2017-03-01 10:39:03 -0500

    cleanup

--- duplicates.py
+++
@@ -1,9 +0,0 @@
-#find duplicates in array
-def find_duplicates(arr):
-    dup_arr = arr[:]
-    for i in set(arr):
-        dup_arr.remove(i)
-    
-    return list(set(dup_arr))   
-
-print(find_duplicates([1,2,2,3,4,4]))
--- module.js
+++
@@ -1,7 +0,0 @@
-export function cube(x){
-    return x * x;
-}
-
-var foo = Math.PI + Math.SQRT2;
-
-export {cube, foo};
